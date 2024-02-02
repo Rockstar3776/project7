82dac779cc6fbdb6e6a90c79ae3000f4b98db846
Rockstar3776 <157124140+Rockstar3776@users.noreply.github.com>
2024-02-02 09:42:04 +0000

    precision dashboard : id client focus

--- dashboard/main.py
+++ dashboard/main.py
@@ -4,22 +4,47 @@ import json
 import pandas as pd
 import numpy as np
 
-st.title('OpenClassrooms Project 7')
-st.title('_Léa_ is :blue[awesome] :sunglasses:')
+st.title('Dashboard prédiction client')
 
+# Asking client ID
 
 df = pd.read_csv("data/data.csv").set_index(keys=["SK_ID_CURR"])
+list_options = list(df.index)
+option = st.selectbox(
+    'Pour quelle personne voulez vous voir les résultats ?',
+    (list_options))
 
-st.dataframe(df)  # Same as st.write(df)
+
+st.table(df.loc[option,:].T)  # Show the data of the client
 
 st.divider()
 
-value_imput = st.text_input('ID client', value='', key=int)
-st.write("L'ID du client est le :", value_imput)
+#chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+#st.line_chart(chart_data)
 
-option = st.selectbox(
-    'Pour quelle personne voulez vous voir les résultats ?',
-    ('Paul', 'Léa', 'Autres'))
+
+st.divider()
+
+list_columns = ['EXT_SOURCE_3',
+                'EXT_SOURCE_2',
+                'EXT_SOURCE_1',
+                'CODE_GENDER',
+                'DAYS_EMPLOYED',
+                'NAME_EDUCATION_TYPE',
+                'AMT_CREDIT',
+                'DAYS_BIRTH']
+
+option_col = st.selectbox(
+    'Pour quelle catégorie voulez vous voir les résultats ?',
+    (list_columns))
+
+chart_data = df[option_col]
+colors = ['red' if idx == option else 'blue' for idx in chart_data.index]
+
+st.bar_chart(chart_data, c=colors)
+
+
+
 
 st.write('Vous avez sélectionné:', option)
 
@@ -27,5 +52,5 @@ def call_api(id: str) -> dict:
     r = requests.get('http://127.0.0.1:8000/models-results', params={"id": id})
     return r.json()
 
-model_result = call_api(id=value_imput)
+model_result = call_api(id=option)
 st.write(f"Le résultat du model est: {model_result}")
